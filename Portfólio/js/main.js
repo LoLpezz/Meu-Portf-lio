2a0307cb9d1a7fbf5ea8a9f9ec637d239a30daab
Maicon Lopes <89826599+LoLpezz@users.noreply.github.com>
2022-08-15 11:15:49 -0300

    Add files via upload

---
+++ Portfólio/js/main.js
@@ -0,0 +1,9 @@
+/* $(document).ready(function(){
+    $(window).scroll(function(){
+    if ($(this).scrollTop() > 160) {
+            $('.back-to-top').fadeIn(250);
+    }else {
+            $('.back-to-top').fadeOut(250);
+    }
+    })
+}) */
